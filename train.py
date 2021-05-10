f3694be42a52fb78a5ec4f79f68989c2d2581624
root <sunilkumar.ag@philips.com>
2021-05-10 18:07:11 +0530

    Updated change

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 
 #if you need to install some packages, use this command:
 #!pip install tensorflow
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
 
 #read the Dataset
 salary_df = pd.read_csv('Salary_Data.csv')
@@ -20,6 +22,15 @@ y = salary_df[['Salary']]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
 #need to have y_train & y_test as an vector(for SageMaker linear learner) 
-y_train = y_train[:,0]
-y_test = y_test[:,0]
+#y_train = y_train[:,0]
+#y_test = y_test[:,0]
+y_train = y_train.iloc[:,0].values
+y_test = y_test.iloc[:,0].values
 
+#pass in the training data from S3 to train the linear learner model
+train = lin_reg.fit(X_train,y_train)
+print("fit resyults", train)
+print("Training done")
+results = lin_reg.predict(X_test)
+print("Prediction results",results)
+print("Prediction done")
